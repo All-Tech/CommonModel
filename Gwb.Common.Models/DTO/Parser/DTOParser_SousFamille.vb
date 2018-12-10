62bfd52cd101ed12a25a0d02224a894bc2cd0e34
benjamin.essam <benjamin.essam@ALLTECH-DEV02>
2018-12-10 19:42:23 +0100

    Update DTO 10-12-2018  19:41

---
+++ Gwb.Common.Models/DTO/Parser/DTOParser_SousFamille.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Data.Common
+Imports Gwb.Common.Models.CommonBase
+
+Namespace DTO
+    Public Class DTOParser_SousFamille
+        Inherits DTOParser
+
+        Public Overrides Sub PopulateOrdinals(reader As DbDataReader)
+            Throw New NotImplementedException()
+        End Sub
+
+        Public Overrides Function PopulateDTO(reader As DbDataReader) As DTOBase
+            Throw New NotImplementedException()
+        End Function
+    End Class
+End Namespace
---
+++ Gwb.Common.Models/DTO/SousFamille.vb
@@ -0,0 +1,12 @@
+﻿Imports Gwb.Common.Models.CommonBase
+
+Namespace DTO
+    Public Class SousFamille
+        Inherits DTOBase
+        Public Property SousFamilleID As Integer
+        Public Property FamilleID As Integer
+        Public Property Code As String
+        Public Property Libelle As String
+    End Class
+End Namespace
+
